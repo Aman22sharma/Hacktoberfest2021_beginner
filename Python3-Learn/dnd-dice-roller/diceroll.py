56f5a6d48342e5f7a1ff6c29579fd62705f0a0a9
Russ Carroll <russ.carroll@pm.me>
2020-10-08 17:12:26 +0000

    Add beginnings of a tabletop RPG dice roller module.

---
+++ Python3-Learn/dnd-dice-roller/diceroll.py
@@ -0,0 +1,24 @@
+import random
+
+def diceroll(quantity:int, sides:str):
+    if sides == "d4":
+        die = 4
+    elif sides == "d6":
+        die = 6
+    elif sides == "d10":
+        die = 10
+    elif sides == "d12":
+        die = 12
+    elif sides == "d20":
+        die = 20
+    elif sides == "d100":
+        die = 100
+    else:
+        print("That is not a die type")
+
+    for i in range(quantity):
+        result = (random.randint(1, die))
+        print(i, sides, " = ", result)
+        total = total + result
+    print("Roll result: ", total)
+
