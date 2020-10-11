693d07881bc17a1f46c6d4db2edf81e605a99896
Mayank Khurmai <mayankkhurmai8@gmail.com>
2020-10-11 11:16:35 +0530

    Find IP Address Using the Pyhton Code

---
+++ Python3-Learn/Find-IP-Address-Using-Pyhton.py
@@ -0,0 +1,5 @@
+import socket    
+hostname = socket.gethostname()    
+IPAddr = socket.gethostbyname(hostname)    
+print("Your Computer Name is:" + hostname)    
+print("Your Computer IP Address is:" + IPAddr) 
