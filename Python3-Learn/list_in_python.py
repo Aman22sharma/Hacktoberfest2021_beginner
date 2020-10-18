980f8cd2999b0b5436026169f75defab9f99b351
manasisaiba <manasisaiba99@gmail.com>
2020-10-18 11:51:59 +0530

    added the list concept in python

---
+++ Python3-Learn/list_in_python.py
@@ -0,0 +1,18 @@
+// AUTHOR: Manasi Saiba
+// Python3 Concept: LIST IN PYTHON
+// GITHUB: https://github.com/manasisaiba
+
+#Python offers a range of compound data types often referred to as sequences.
+# List is one of the most frequently used and very versatile data types used in Python.
+
+# empty list
+my_list = []
+
+# list of integers
+my_list = [1, 2, 3]
+
+# list with mixed data types
+my_list = [1, "Hello", 3.4]
+#A list can also have another list as an item. This is called a nested list.
+# nested list
+my_list = ["mouse", [8, 4, 6], ['a']]
