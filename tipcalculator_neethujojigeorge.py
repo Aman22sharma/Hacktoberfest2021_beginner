da7d4358a237e01f78710f7c4088ff7c4d52559f
NeethuJojiGeorge <55632660+NeethuJojiGeorge@users.noreply.github.com>
2020-10-23 19:49:00 +0530

    Create tipcalculator_neethujojigeorge.py
    Total Bill amount along with Tip Calculator using python

---
+++ tipcalculator_neethujojigeorge.py
@@ -0,0 +1,18 @@
+
+# AUTHOR: Neethu Joji George
+# Python3 Concept: Tip Calculator Using Python
+# GITHUB: https://github.com/NeethuJojiGeorge
+
+#tip calculator code
+print("Bill Calculator With the Tip")
+
+def gettip(percentage, sub_total):
+    tip = ((sub_total * percentage) / 100)
+    total = (sub_total + tip)
+    return total
+
+sub_total = int(input("How much is the bill?"))
+
+percentage = float(input("What percentage should the tip be?"))
+
+print (gettip(percentage, sub_total))
