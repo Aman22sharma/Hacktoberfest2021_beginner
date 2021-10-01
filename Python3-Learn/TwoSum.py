3f94c10b1be91c30e67dc1978b104816d22f785b
gympohnpimol <gympohnpimol@gmail.com>
2021-10-01 22:10:31 +0700

    add TwoSum.py

---
+++ Python3-Learn/TwoSum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        ls = []
+        for i in range(0, len(nums)):
+            item = target - nums[i]
+            nums[i] = "done"
+            if item in nums:
+                ls.append(i)
+                ls.append(nums.index(item))
+        return ls
