2485788f4a81c37e5dd29a75e2d73cf788ac4f23
Dhruv1328 <91804799+Dhruv1328@users.noreply.github.com>
2021-10-03 03:07:45 +0530

    A percentage calculator for python
    Please view the pr and merge accordingly

---
+++ marks_percentage_calculator.py
@@ -0,0 +1,25 @@
+print("Enter Marks Obtained in 5 Subjects: ")
+
+mOne = int(input())
+
+mTwo = int(input())
+
+mThree = int(input())
+
+mFour = int(input())
+
+mFive = int(input())
+
+sum = mOne+mTwo+mThree+mFour+mFive
+
+avg = sum/5
+
+perc = (sum/500)*100
+
+print(end="Average Mark = ")
+
+print(avg)
+
+print(end="Percentage Mark = ")
+
+print(perc)
