9c15df14bff481f73ddd5b949b28600a9000c34c
Tanmay Khandelwal <58903502+Tanmay-901@users.noreply.github.com>
2021-10-02 22:02:32 +0530

    Create Josephus-problem_Tanmay-901.py

---
+++ Python3-Learn/Josephus-problem_Tanmay-901.py
@@ -0,0 +1,19 @@
+#Josephus Problem
+
+# The Josephus problem, where 100 people standing in a circle, 1 kills 2 passes the sword to 3….who survives in the end?
+
+#Python Solution
+
+from sys import stdin,stdout                          
+
+def Josephus_Prblm(n, k):
+    if (n == 1): 
+       return 1
+    else: 
+       return ((Josephus_Prblm(n - 1, k) + k-1) % n + 1)
+
+for _ in range(int(stdin.readline())):
+	N = int(stdin.readline())
+  K = int(stdin.readline())
+  print(Josephus_Prblm(N,K)
+
