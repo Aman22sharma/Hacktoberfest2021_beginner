d7e1cdfe57dd272f01ebc1f96084eb032666e73b
Gourav <71971801+gouravchawla334@users.noreply.github.com>
2021-10-04 17:01:03 +0530

    Add files via upload

---
+++ Python3-Learn/ATM_gouravchawla334.py
@@ -0,0 +1,48 @@
+while True:
+    balance=10000;
+    print("                         <<--        Welcome to ATM      -->>                         ")
+    print("""
+    1)          Balance
+    2)          Withdraw
+    3)          Deposit
+    4)          Quit
+    """)
+
+    try:
+        Option=int(input("Enter Option :"))
+
+    except Exception as e:
+        print("Error:",e)
+        print("Enter 1,2,3 or 4 only")
+        continue
+
+    if Option==1:
+        print("Balance $ ",balance)
+
+    if Option==2:
+        print("Balance $ ",balance)
+        Withdraw=float(input("Enter Withdraw ammount $ :"))
+
+        if Withdraw>0:
+            forwardbalance=(balance-Withdraw)
+            print("Forward balance is $ ",forwardbalance)
+
+        elif Withdraw>balance:
+            print("No Balance in account !!!")
+
+        else:
+            print("None withdraw made ")
+
+    if Option==3:
+        print("Balance $ ",balance)
+        Deposit=float(input("Enter deposit ammount $ :"))
+
+        if Deposit>0:
+            forwardbalance=(balance+Deposit)
+            print("forwardbalance $",forwardbalance)
+
+        else:
+            print("No deposit made !!!")
+    
+    if Option==4:
+        exit()
