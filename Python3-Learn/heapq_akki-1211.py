dbe394b16568d53ab952f8212132050c9c9d042e
akki-1211 <parekhakshat1211@gmail.com>
2021-10-04 12:40:27 +0530

    Create heapq_akki-1211.py

---
+++ Python3-Learn/heapq_akki-1211.py
@@ -0,0 +1,27 @@
+# Python code to demonstrate working of
+# heapify(), heappush() and heappop()
+
+# importing "heapq" to implement heap queue
+import heapq
+
+# initializing list
+li = [5, 7, 9, 1, 3, 6, 10]
+
+# using heapify to convert list into heap
+heapq.heapify(li)
+
+# printing created heap
+print ("The created heap is : ",end="")
+print (list(li))
+
+# using heappush() to push elements into heap
+# pushes 4
+heapq.heappush(li,4)
+
+# printing modified heap
+print ("The modified heap after push is : ",end="")
+print (list(li))
+
+# using heappop() to pop smallest element
+print ("The popped and smallest element is : ",end="")
+print (heapq.heappop(li))
