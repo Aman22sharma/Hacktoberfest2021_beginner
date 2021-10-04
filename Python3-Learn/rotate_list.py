cb1ac960026d2b56bcfe282dccf8be4a52ca9490
Mourya Pradeep Ramashare <53226424+prm1999@users.noreply.github.com>
2021-10-04 19:55:04 +0530

    Update rotate_list.py
    updated

--- Python3-Learn/rotate_list.py
+++ Python3-Learn/rotate_list.py
@@ -1,3 +1,9 @@
+# AUTHOR: prm1999
+# Python3 Concept: Rotate the word in the list.
+# GITHUB: https://github.com/prm1999
+
+# Function 
+
 def rotate(arr, n):
     n = len(arr)
     i = 1
@@ -8,6 +14,9 @@ def rotate(arr, n):
     arr[0] = x
 
     return arr
+
+
+#main
 
 A = [1, 2, 3, 4, 5]
 a=rotate(A,5)
