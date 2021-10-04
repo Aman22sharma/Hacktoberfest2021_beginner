3454bbf7cc093cedbc84a306a1c3a824a8a8eb60
Sanket khadse <edusanketdk@gmail.com>
2021-10-04 12:34:18 +0530

    added matrix_spiral_traversal_edusanketdk.py

---
+++ Python3-Learn/matrix_spiral_traversal_edusanketdk.py
@@ -0,0 +1,24 @@
+def sol_1(mat: list) -> list:
+    u, r, d, l, dir = 0, len(mat[0]), len(mat), 0, 0
+    ans = []
+
+    while u <= d and l <= r:
+        if dir == 0:
+            for i in range(l, r+1):
+                ans.append(mat[u][i])
+            u += 1
+        elif dir == 1:
+            for i in range(u, d+1):
+                ans.append(mat[i][r])
+            r -= 1
+        elif dir == 2:
+            for i in range(r, l-1, -1):
+                ans.append(mat[d][i])
+            d -= 1
+        else:
+            for i in range(d, u-1, -1):
+                ans.append(mat[i][l])
+            l += 1
+        dir = (dir + 1)%4
+
+    return ans
