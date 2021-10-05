8cef91bd80f11f19f5b30565b1aeb57dbd4e55a5
aditya <yzendric@gmail.com>
2021-10-05 12:23:28 +0530

    Edited YouTubeSubsPredictor/main.py as requested

--- YouTubeSubsPredictor/main.py
+++ YouTubeSubsPredictor/main.py
@@ -1,65 +1,79 @@
+# AUTHOR: Aditya Singh
+# Python3 Concept: Machine Learning to predict Youtube Subcribers 
+# GITHUB: https://github.com/YusiferZendric
+
 import numpy as np
-import matplotlib.pyplot as plt
-from numpy.lib.shape_base import column_stack
-import pandas as pd
-import random
-from pandas.core.frame import DataFrame
-from sklearn.preprocessing import MinMaxScaler
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, LSTM
-from tensorflow.python.keras.utils.tf_utils import dataset_is_infinite
-main_list = []
-def main(months, datalist):
-    for i in range(months):
 
-        df = pd.read_csv("random_dataset.csv")
-        total_rows = df['Subs_Gained'].count() 
-        scaler = MinMaxScaler(feature_range=(0,1))
-        scaled_data = scaler.fit_transform(df["Subs_Gained"].values.reshape(-1,1))   
-        prediction_week = 12
 
-        x_train = []
-        y_train = []
+def retroactive_resolution(coefficients: np.matrix, vector: np.ndarray) -> np.ndarray:
+    """
+    This function performs a retroactive linear system resolution
+        for triangular matrix
+    Examples:
+        2x1 + 2x2 - 1x3 = 5         2x1 + 2x2 = -1
+        0x1 - 2x2 - 1x3 = -7        0x1 - 2x2 = -1
+        0x1 + 0x2 + 5x3 = 15
+    >>> gaussian_elimination([[2, 2, -1], [0, -2, -1], [0, 0, 5]], [[5], [-7], [15]])
+    array([[2.],
+           [2.],
+           [3.]])
+    >>> gaussian_elimination([[2, 2], [0, -2]], [[-1], [-1]])
+    array([[-1. ],
+           [ 0.5]])
+    """
 
-        for x in range(prediction_week, len(scaled_data)):
-            x_train.append(scaled_data[x-prediction_week:x, 0])
-            y_train.append(scaled_data[x, 0])
+    rows, columns = np.shape(coefficients)
 
-        x_train, y_train = np.array(x_train), np.array(y_train)
-        x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+    x = np.zeros((rows, 1), dtype=float)
+    for row in reversed(range(rows)):
+        sum = 0
+        for col in range(row + 1, columns):
+            sum += coefficients[row, col] * x[col]
 
-        model = Sequential()
+        x[row, 0] = (vector[row] - sum) / coefficients[row, row]
 
-        model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
-        model.add(Dropout(0.2))
-        model.add(LSTM(units=50, return_sequences=True))
-        model.add(Dropout(0.2))
-        model.add(LSTM(units=50))
-        model.add(Dropout(0.2))
-        model.add(Dense(units=1))
+    return x
 
-        model.compile(optimizer='adam', loss='mean_squared_error')
-        model.fit(x_train, y_train, epochs=100, batch_size=32)
 
-        model_inputs = df[len(df) - prediction_week:].values
-        model_inputs = model_inputs.reshape(-1,1)
-        model_inputs = scaler.transform(model_inputs)
+def gaussian_elimination(coefficients: np.matrix, vector: np.ndarray) -> np.ndarray:
+    """
+    This function performs Gaussian elimination method
+    Examples:
+        1x1 - 4x2 - 2x3 = -2        1x1 + 2x2 = 5
+        5x1 + 2x2 - 2x3 = -3        5x1 + 2x2 = 5
+        1x1 - 1x2 + 0x3 = 4
+    >>> gaussian_elimination([[1, -4, -2], [5, 2, -2], [1, -1, 0]], [[-2], [-3], [4]])
+    array([[ 2.3 ],
+           [-1.7 ],
+           [ 5.55]])
+    >>> gaussian_elimination([[1, 2], [5, 2]], [[5], [5]])
+    array([[0. ],
+           [2.5]])
+    """
+    # coefficients must to be a square matrix so we need to check first
+    rows, columns = np.shape(coefficients)
+    if rows != columns:
+        return np.array((), dtype=float)
 
-        real_data = [model_inputs[len(model_inputs)+1 - prediction_week:len(model_inputs+1), 0]]
-        real_data = np.array(real_data)
-        real_data = np.reshape(real_data, (real_data.shape[0], real_data.shape[1],1))
+    # augmented matrix
+    augmented_mat = np.concatenate((coefficients, vector), axis=1)
+    augmented_mat = augmented_mat.astype("float64")
 
-        prediction = model.predict(real_data)
-        prediction = scaler.inverse_transform(prediction)
-        prediction = round(prediction[0][0])
-        new_week = total_rows + i + 1
-        to_append = [new_week, prediction]
-        datalist.append(to_append)
-        print(i+1)
-    return datalist
+    # scale the matrix leaving it triangular
+    for row in range(rows - 1):
+        pivot = augmented_mat[row, row]
+        for col in range(row + 1, columns):
+            factor = augmented_mat[col, row] / pivot
+            augmented_mat[col, :] -= factor * augmented_mat[row, :]
 
-#main = main(4, main_list)
-#subs = 0
-#for i in main:
-#    subs+=i[1]
-#print(subs)
+    x = retroactive_resolution(
+        augmented_mat[:, 0:columns], augmented_mat[:, columns : columns + 1]
+    )
+
+    return x
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
