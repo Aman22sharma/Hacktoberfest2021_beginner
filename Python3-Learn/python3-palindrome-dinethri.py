6059512c0527ccdf9c7ead01b5047e4e28bfe883
Dinethri <desilvadinethri@gmail.com>
2021-10-05 21:18:26 +0530

    Add concept changes

---
+++ Python3-Learn/python3-palindrome-dinethri.py
@@ -0,0 +1,16 @@
+
+print("Enter the Number ")
+num = int(input())
+temp = num
+reverse = 0
+
+while(num>0):
+    dig = num%10
+    reverse = reverse*10+dig
+    num = num//10
+
+print(reverse)
+if temp==reverse:
+    print("Number is in Palindrome")
+else:
+    print("Number is not in Palindrome")
