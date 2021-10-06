174bdfbe3d73c652dda9a087ea005a412c0935f8
Akash Rajak <57003737+akash435@users.noreply.github.com>
2021-10-06 23:17:20 +0530

    Added matrix_transpose.py

---
+++ Python3-Learn/matrix_transpose.py
@@ -0,0 +1,32 @@
+
+# Matrix Transpose Program
+
+def matrix_transpose():
+	for i in range(len(A)):
+		for j in range(len(A[0])):
+			res[i][j] = A[j][i]
+
+A=[]
+
+print("Enter N:")
+n = int(input())
+
+print("Enter Matrix A:")
+for i in range(0, n):
+	temp = []
+	for j in range(0, n):
+		x = int(input())
+		temp.append(x)
+	A.append(temp)
+
+res = []
+for i in range(0, n):
+	temp = []
+	for j in range(0, n):
+		temp.append(0)
+	res.append(temp)
+
+matrix_transpose()
+
+for r in res:
+	print(r)
