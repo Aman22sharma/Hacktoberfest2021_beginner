d88367618ddd78e9d543bf8610b72d29e8d4b047
Chamudi Rasanjalee <91789995+chamz99@users.noreply.github.com>
2021-10-07 12:24:37 +0530

    update as a effective calculator

--- Python3-Learn/Hello-World_erarijit.py
+++ Python3-Learn/Hello-World_erarijit.py
@@ -3,3 +3,28 @@
 // GITHUB: https://github.com/erarijit
 
 print("Hello World! Welcome to Github")
+print("well come to the calculator")
+import numpy as np
+a='addition'
+s='subraction'
+m='multiplication'
+d='division'
+calc=input('yo want addition(a), Subtraction(s), Multiplication(m), or division(d)?')
+if calc =='a':
+    num1=float(input(''))
+    num2=float(input(''))
+    print(num1+num2)
+elif calc== 's':
+    num1=float(input(''))
+    num2=float(input(''))
+    print(num1-num2)
+elif calc=='m':
+    num1=float(input(''))
+    num2=float(input(''))
+    print(num1*num2)
+elif calc=='d':
+    num1=float(input(''))
+    num2=float(input(''))
+    print(num1/num2)
+else:
+    print('Please use the letters in paranthesis to indicate the operation')
