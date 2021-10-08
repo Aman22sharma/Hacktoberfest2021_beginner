eaaf8109139b1ac0ced8114e6398717c4792d1fb
Akshit <61937872+homeboy445@users.noreply.github.com>
2021-10-08 19:26:46 +0530

    Update and rename PDFtoSpeech_homeboy445.py to python3-PDFtoSpeech_homeboy445.py
    Let me know if anything's missing. Thanks!

--- Python3-Learn/python3-PDFtoSpeech_homeboy445.py
+++ Python3-Learn/python3-PDFtoSpeech_homeboy445.py
@@ -2,17 +2,19 @@
 # Python3 Concept: PDF to text and text narration
 # GITHUB: https://github.com/homeboy445
 
-# PDF reader:
-# Let your computer do the story telling!
+'''
+PDF reader:
+Let your computer do the story telling!
 
-# How to get started?
-# Note: Make sure you have python installed 
-# in your system.
+How to get started?
+Note: Make sure you have python installed 
+in your system.
 
-# Linux: pip install pyttsx3 && pip install pdftotext && python3 Mainer.py
-# Windows: pip install pyttsx3, pip install pdftotext, python3 Mainer.py
+Linux: pip install pyttsx3 && pip install pdftotext && python3 Mainer.py
+Windows: pip install pyttsx3, pip install pdftotext, python3 Mainer.py
 
-# Note: If this package "pdftotext" doesn't seem to work and/or you couldn't find it! please search online on how to install this on your respective machine...
+Note: If this package "pdftotext" doesn't seem to work and/or you couldn't find it! please search online on how to install this on your respective machine...
+'''
 
 import pyttsx3
 import pdftotext
