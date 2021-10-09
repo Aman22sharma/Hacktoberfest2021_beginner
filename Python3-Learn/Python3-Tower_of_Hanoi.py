63c8709d6f9fbecae45f8be3e8cd852a584c2cd4
Rohanrbharadwaj <89947037+Rohanrbharadwaj@users.noreply.github.com>
2021-10-10 04:11:36 +0530

    Create Python3-Tower_of_Hanoi.py

---
+++ Python3-Learn/Python3-Tower_of_Hanoi.py
@@ -0,0 +1,16 @@
+'''Name : Rohan Rajeev Bharadwaj
+Python3 Concept : Functions, Recursion
+Github Profile Link : https://github.com/Rohanrbharadwaj'''
+
+
+def tower_of_hanoi(n, source, destination, auxiliary):
+    if(n==1):
+        print("Move disc 1 from source", source, "from to destination", destination)
+    else: 
+        tower_of_hanoi(n-1, source, auxiliary, destination)
+        print("Move disc", n, "from source", source,"from to destination", destination)
+        tower_of_hanoi(n-1, auxiliary, destination, source)
+
+n_disks=int(input("Please enter the number of disks\n"))
+
+tower_of_hanoi(n_disks, "A", "C", "B")
