46545c0dd2d9c7d92fc68cf3d2e97fb4add0ca9b
ASHUTOSH KUMAR CHOUDHARY <choudharykumarashutosh1209@gmail.com>
2021-10-24 19:10:17 +0530

    Create python3-data_types_Ashutosh Kumar Choudhary.py

---
+++ Python3-Learn/python3-data_types_Ashutosh Kumar Choudhary.py
@@ -0,0 +1,15 @@
+// AUTHOR: Ashutosh Kumar Choudhary
+// Python3 Concept: All data types in python 
+// GITHUB: https://github.com/Ash-KODES
+    
+Data types are the classification or categorization of data items. It represents the kind of value that tells what operations can be performed on a particular data. Since everything is an object in Python programming, data types are actually classes and variables are instance (object) of these classes.
+
+Following are the standard or built-in data type of Python:
+
+-Numeric
+-Sequence Type
+-Boolean
+-Set
+-Dictionary
+
+
