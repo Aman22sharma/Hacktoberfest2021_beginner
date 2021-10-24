9ff1e916aeb148d46bca347be867eabd8e726621
rahmah18 <56541229+rahmah18@users.noreply.github.com>
2021-10-24 22:59:34 +0100

    Update Fibonacci-VishnuRamV.py
    Fibonacci function

--- Python3-Learn/Fibonacci-VishnuRamV.py
+++ Python3-Learn/Fibonacci-VishnuRamV.py
@@ -20,3 +20,15 @@ else:
        n1 = n2
        n2 = nth
        count += 1
+         
+         
+def fib():
+     n = int(input())
+     a = 0
+     b = 1
+     p = []
+     for i in range(n):
+         p.append(a)
+         a,b = b,a+b
+     print(p)
+fib()
