662174e851b2e94cd098ffff86f8e09bd655fa6e
Raunak Kumar <52267072+Raunakkumarr@users.noreply.github.com>
2021-10-27 16:14:24 +0545

    Update linkedInScrapper.py

--- Python3-Learn/LinkedIn Job Scrapper/linkedInScrapper.py
+++ Python3-Learn/LinkedIn Job Scrapper/linkedInScrapper.py
@@ -13,18 +13,29 @@ def find_jobs():
     if "," in keyword:
         keyword = keyword.replace(",","%2C")
     final_url = initial_url+"keywords="+keyword+"&location="+location+"&geoId=&trk=homepage-jobseeker_jobs-search-bar_search-submit&position=1&pageNum=0"
-    print(final_url)
+    #print(final_url)
     address = requests.get(final_url)
     src = address.content
     soup = BeautifulSoup(src, 'lxml')
     notes = soup.find_all('a', class_="base-card__full-link")
     #print(notes)
-    for i in notes:
-        link=i['href']
-        #print(link)
-        parent = i.find_parent()
-        print(parent.find('h3').text)
-        #print(parent.find('h4').text)
-        #print(parent.find('span',class_="job-search-card__location").text)
-        #print(parent.find('time')['datetime'])
+    final_url = initial_url+"keywords="+keyword+"&location="+location+"&geoId=&trk=homepage-jobseeker_jobs-search-bar_search-submit&position=1&pageNum=0"
+    address = requests.get(final_url)
+    src = address.content
+    soup = BeautifulSoup(src, 'lxml')
+    jobs = soup.find_all('a', class_="base-card__full-link")
+    for i in range(0,10):
+      job = jobs[i]
+      link=job['href']
+      parent = job.find_parent()
+      title = parent.find('h3').text
+      company = parent.find('h4').text
+      location = parent.find('span',class_="job-search-card__location").text
+      posted_on = parent.find('time')['datetime']
+      print("Title: ",title)
+      print("Company: ",company)
+      print("Location: ",location)
+      print("Posted On: ",posted_on)
+      print("More Info:", link)
+      
 find_jobs()
