54f796e5989264fba09699aa24b33a9475694d42
Akash singh <83067464+kshatriyaakash@users.noreply.github.com>
2021-10-30 14:45:21 +0530

    loops in python

---
+++ Loops.py
@@ -0,0 +1,9 @@
+# Python For Loop : the for loop in python is used to iterate over a sequence(list,tuple string) or other iterable objects.
+
+# iterating over a sequence is called traversal
+
+
+#for loops with list
+a=['apple','mango','banana','orange']
+for name in a:
+    print(name *2)  # when a string is multiplied to any integer it gets itself repeated that many times
