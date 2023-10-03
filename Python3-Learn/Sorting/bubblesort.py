b878522f285d38325be2d22b3bb7f6015d9b6b16
Mainak <chatterjeemainak2003@gmail.com>
2023-10-03 18:03:34 +0530

    commit message

---
+++ Python3-Learn/Sorting/bubblesort.py
@@ -0,0 +1,16 @@
+# Bubble sort
+
+def bubbleSort(arr):
+	n = len(arr)
+	swapped = False
+	for i in range(n-1):
+		for j in range(0, n-i-1):
+			if arr[j] > arr[j + 1]:
+				swapped = True
+				arr[j], arr[j + 1] = arr[j + 1], arr[j]		
+		if not swapped:
+			return
+
+arr = [64, 34, 25, 12, 22, 11, 90]
+bubbleSort(arr)
+print("Sorted array is:\n",arr)
